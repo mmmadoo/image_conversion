1bc4b216837e64a78f93041f9a1c7ab1f319bce2
mmmadoo <taisei02220@gmail.com>
2025-05-25 20:20:21 +0900

    Update function.py

--- function.py
+++ function.py
@@ -14,8 +14,9 @@ def image_combination(alist):
     for i in range(len(alist)):
         img.paste(alist[i],(0,update_height))
         update_height += alist[i].height
-    
+        
     return img
+
 # 変換前のファイルをフォルダから削除する
 def delete_before_file(name_alist):
     for name in name_alist:
@@ -23,28 +24,33 @@ def delete_before_file(name_alist):
 
 # ファイルをストリームとして送信
 def generate(file_path):
-    with open(file_path, 'rb') as f:
+    with open(file_path,'rb') as f:
         yield from f
 
+#ファイルのExif情報から画像ファイルの向きを修正する
 def correct_image_orientation(img):
     try:
         exif = img._getexif()
         if exif is not None:
-            for orientation in ExifTags.TAGS.keys():
-                if ExifTags.TAGS[orientation] == 'Orientation':
+            orientation = None
+            for key in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[key] == 'Orientation':
+                    orientation = key
                     break
-            orientation_value = exif.get(orientation, None) # type: ignore
-            if orientation_value == 3:
-                img = img.rotate(180, expand=True)
-            elif orientation_value == 6:
-                img = img.rotate(270, expand=True)
-            elif orientation_value == 8:
-                img = img.rotate(90, expand=True)
+            if orientation is not None:
+                orientation_value = exif.get(orientation,None)
+                if orientation_value == 3:
+                    img = img.rotate(180,expand = True)
+                elif orientation_value == 6:
+                    img = img.rotate(270,expand = True)
+                elif orientation_value == 8:
+                    img = img.rotate(90,expand = True)
     except Exception:
         pass
     return img
 
-def save_uploaded_image(file, save_path):
+#ファイルの向きを修正して、保存する
+def save_uploaded_image(file,file_name):
     img = Image.open(file)
     img = correct_image_orientation(img)
-    img.save(save_path)
+    img.save(file_name)
